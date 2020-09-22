f4bc3ec1d3b8f319d3bef895b83dd4b78dc51d6f
Kseniya Belyavtseva <kseniyabelyavtseva@protonmail.com>
2020-09-22 17:41:31 +0300

    solve tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,19 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  str = str.toString();
-  options.addition = options.addition.toString();
-  let modifiedStr = str + options.addition;
-  
-  let repeatTime = options.repeatTime ? options.repeatTime || 0;
-  for (let i = 0; i < repeatTime; i++) {
+module.exports = 
+function repeater(str, options) {
+  let modified = []; // храним все добавляемые эелементы
+  let repeat = options.repeatTimes ? options.repeatTimes : 1;
+  const newStr = String(str); // убедились, что строка является строкой
+  const separator = options.separator ? options.separator : '+';
+  const addition = (options.addition === undefined) ? '' : String(options.addition); //???
+  const additionSep = options.additionSeparator ? options.additionSeparator : '|';
 
+  while (repeat > 0) {
+    modified.push(newStr);
+
+    let i = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+
+    while (i > 0) {
+      modified.push(addition);
+      if (i > 1) {
+        modified.push(additionSep);
+      }
+      i--;
+    }
+
+    if (repeat > 1) {
+      modified.push(separator);
+    }
+
+    repeat--
   }
-// options is an object of options, that contains properties:
-//    repeatTimes: sets the number of repetitions of the str;
-//    separator: is a string separating repetitions of the str;
-//    addition: is an additional string that will be added to each repetition of the str;
-//    additionRepeatTimes: sets the number of repetitions of the addition;
-//    additionSeparator: is a string separating repetitions of the addition.
-}
+
+  return modified.filter(elem => elem !== '').join('')
   
+}
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
+
+
 const chainMaker = {
+  chains: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof value === 'undefined') {
+      this.chains.push('()');
+    }
+    else {
+      this.chains.push(String(value));
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > this.chains.length) {
+      throw 'Error';
+    }
+    else {
+      this.chains[position - 1] = null;
+      this.chains = this.chains.filter(el => el !== null);
+      return this
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains = this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    for (let i = 0; i < this.chains.length; i++) {
+      if (i !== this.chains.length - 1) {
+        str += `(${this.chains[i]})~~`;
+      }
+      else {
+        str += `(${this.chains[i]})`;
+      }
+    }
+    return str
   }
 };
 
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain())
 module.exports = chainMaker;
