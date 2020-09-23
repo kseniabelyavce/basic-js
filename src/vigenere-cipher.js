c246a7cf2528887e53056c8e046662511eeb8608
Kseniya Belyavtseva <kseniyabelyavtseva@protonmail.com>
2020-09-24 00:14:59 +0300

    solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.value = value;
+  }
+
+  encrypt(message, key) {
+    let cipher = "";
+    if (message === undefined || key === undefined) {
+      throw 'Error';
+    }
+    
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let upperCaseLetter = message[i].toUpperCase();
+
+      if (upperCaseLetter.charCodeAt() >= 65 && upperCaseLetter.charCodeAt() <= 90) {
+        if (j > key.length -1 ) {
+          j = 0;
+        }
+        let currentLetter = ((upperCaseLetter.charCodeAt() - 65) + (key[j].toUpperCase().charCodeAt() - 65)) % 26; 
+        cipher += String.fromCharCode(currentLetter + 65);
+        j++;
+      } else {
+        cipher += upperCaseLetter;
+      }
+    }
+    if (this.value === false) {
+      return cipher.split('').reverse().join('');
+    }
+    return cipher;
+  }
+
+  decrypt(message, key) {
+    let cipher = "";
+    if (message === undefined || key === undefined) {
+      throw 'Error';
+    }
+    
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let letter = message[i]
+
+      if (letter.charCodeAt() >= 65 && letter.charCodeAt() <= 90) {
+        if (j > key.length - 1 ) {
+          j = 0;
+        }
+        let currentLetter = ((letter.charCodeAt() + 65) - (key[j].toUpperCase().charCodeAt() - 65)) % 26; 
+        cipher += String.fromCharCode(currentLetter + 65);
+        j++;
+      } else {
+        cipher += letter;
+      }
+    }
+    if (this.value === false) {
+      return cipher.split('').reverse().join('');
+    }
+    return cipher;
   }
 }
 
